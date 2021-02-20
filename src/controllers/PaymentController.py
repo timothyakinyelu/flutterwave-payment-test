b6f2990c5c929b54ad19dd2ecdaa95538966fa46
loverofMush <tolujamal1@gmail.com>
2021-02-20 02:43:59 +0100

    - add payments method post request action

---
+++ src/controllers/PaymentController.py
@@ -0,0 +1,59 @@
+from flask import render_template, json, redirect, request, current_app, url_for
+from datetime import datetime
+import requests
+
+# Flutterwave BaseUrl
+BaseUrl = 'https://api.flutterwave.com/v3'
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {}'.format(current_app.config['SEC_KEY'])
+}
+
+def payments():
+    """Method to fetch form and send payment request payload"""
+    
+    if request.method == 'GET':
+        pass
+    
+    if request.method == 'POST':
+        ref = datetime.timestamp(datetime.now())
+        form = request.form
+        payload = {
+            "tx_ref": ref,
+            "amount": form.get('amount'),
+            "currency": form.get('currency'),
+            "redirect_url": "http://localhost:5000/process",
+            "payment_options": "card",
+            "customer":{
+                "email": form.get('email'),
+                "phonenumber": form.get('phone'),
+                "name": form.get('full_name')
+            },
+            "subaccounts": [
+                {
+                    "id": form.get('subaccountID')
+                }
+            ],
+            "customizations":{
+                "title":"Pied Piper Payments",
+                "description":"Middleout isn't free. Pay the price",
+                "logo":"https://assets.piedpiper.com/logo.png"
+            }
+        }
+        
+        paymentURL = '{}/payments'.format(BaseUrl)
+        
+        try:
+            donate = requests.post(paymentURL, data=json.dumps(payload), headers=headers)
+            response = donate.json()
+            
+            flutterCheckout = response['data']['link']
+            if donate.status_code == 200 and response['status'] == 'success':
+                return redirect(flutterCheckout)
+            else:
+                flash('Unable to process payment')
+                return redirect(url_for('paymentview.payment_view'))
+        except Exception:
+            pass
+        
+        
