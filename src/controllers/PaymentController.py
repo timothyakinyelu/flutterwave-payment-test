b5f8104c784148d7b628721d75db2bb24b80d5cd
loverofMush <tolujamal1@gmail.com>
2021-02-21 22:34:28 +0100

    - import current_user - get retainedCard data - check if user selects retainedCard - send a new payload with card token - save transaction details - changed blueprint name to user - check if card token exists - save if doesn't exist - check if user has a customer_id - save new transaction

--- src/controllers/PaymentController.py
+++ src/controllers/PaymentController.py
@@ -3,6 +3,7 @@ from src.models.transactions import Transaction
 from src.models.cards import Card
 from src.models.users import User
 from datetime import datetime
+from flask_login import current_user
 import requests
 
 # Flutterwave BaseUrl
@@ -33,13 +34,11 @@ def payments():
         ref = datetime.timestamp(datetime.now())
         form = request.form
         
-        fullName = '{} {}'.format(form.get('first_name'), form.get('last_name'))
-        email = form.get('email')
-        phone = form.get('phone')
         currency = form.get('currency')
         amount = form.get('amount')
         subID = form.get('subaccountID')
         donationType = form.get('donation_type')
+        retainedCard = form.get('retained_card')
         
         if int(amount) == False:
             # check if the amount entered is an integer
@@ -55,48 +54,91 @@ def payments():
             #  check if subID is provided
             flash('Please a select a church to donate to!')
             return redirect(url_for('payment.payment_view'))
-
+        
+        
+        if  retainedCard == '1':
+            # existing user can be gotten if a login system is used
+            # if user exists, charge user with token instead
+            card_id = form.get('card_id')
+            card = Card.query.filter_by(id = card_id).first()
+            country = card.country
             
-        payload = {
-            "tx_ref": ref,
-            "amount": amount,
-            "currency": currency,
-            "redirect_url": '{}{}'.format(WebDomain, url_for('payment.verification_view')),
-            "payment_options": "card",
-            "meta":{
-                "reference": donationType
-            },
-            "customer":{
-                "email": email,
-                "phonenumber": phone,
-                "name": fullName
-            },
-            "subaccounts": [
-                {
-                    "id": subID
-                }
-            ],
-            "customizations":{
-                "title":"Pied Piper Payments",
-                "description":"Middleout isn't free. Pay the price",
-                "logo":"https://assets.piedpiper.com/logo.png"
+            data = {
+                "tx_ref": ref,
+                "token": card.token,
+                "currency": currency,
+                "country": country[-2:],
+                "amount": amount,
+                "email": current_user.email,
+                "phone": current_user.phone,
+                "full_name": current_user.full_name,
+                "meta":{
+                    "reference": donationType
+                },
+                "subaccounts": [
+                    {
+                        "id": subID
+                    }
+                ],
             }
-        }
-        
-        paymentURL = '{}/payments'.format(BaseUrl)
-        
-        try:
-            donate = requests.post(paymentURL, data=json.dumps(payload), headers=headers)
-            response = donate.json()
             
-            flutterCheckout = response['data']['link']
-            if donate.status_code == 200 and response['status'] == 'success':
-                return redirect(flutterCheckout)
-            else:
-                flash('Unable to process payment')
-                return redirect(url_for('paymentview.payment_view'))
-        except Exception:
-            pass
+            tokenUrl = '{}/tokenized-charges'.format(BaseUrl)
+            res = requests.post(tokenUrl, data=json.dumps(data), headers=headers)
+            response = res.json()
+            
+            # store transaction ref to check if costumers have any issue with a payment
+            transaction = Transaction(
+                card_id = card.id,
+                user_id = user.id,
+                transaction_ref = transactionRef,
+                status = response['data']['status'],
+                amount = response['data']['amount']
+            )
+            transaction.save()
+            
+            return render_template('response.html', home=url_for('user.verification_view'))
+        else:   
+            # only use if it's a new user or new card
+            payload = {
+                "tx_ref": ref,
+                "amount": amount,
+                "currency": currency,
+                "redirect_url": '{}{}'.format(WebDomain, url_for('user.verification_view')),
+                "payment_options": "card",
+                "meta":{
+                    "reference": donationType
+                },
+                "customer":{
+                    "email": current_user.email,
+                    "phonenumber": current_user.phone,
+                    "name": current_user.full_name
+                },
+                "subaccounts": [
+                    {
+                        "id": subID
+                    }
+                ],
+                "customizations":{
+                    "title":"Pied Piper Payments",
+                    "description":"Middleout isn't free. Pay the price",
+                    "logo":"https://assets.piedpiper.com/logo.png"
+                }
+            }
+            
+            paymentURL = '{}/payments'.format(BaseUrl)
+            
+            try:
+                donate = requests.post(paymentURL, data=json.dumps(payload), headers=headers)
+                response = donate.json()
+                
+                flutterCheckout = response['data']['link']
+                if donate.status_code == 200 and response['status'] == 'success':
+                    return redirect(flutterCheckout)
+                else:
+                    flash('Unable to process payment')
+                    return redirect(url_for('user.payment_view'))
+            except Exception:
+                pass
         
         
 def verify_payment():
@@ -107,7 +149,7 @@ def verify_payment():
             # if request is cancelled by user redirect to main page and flash message
             
             flash('You cancelled the payment!')
-            return redirect('payment.payment_view')
+            return redirect('user.payment_view')
         elif request.args.get('status') == 'successful':
             # if request is successful, verify the transaction
             
@@ -125,35 +167,40 @@ def verify_payment():
                     # store card token generated in order to carry out future payments 
                     # via the token instead of user entering card details
                     # user can be recognised on login to the app
-                    card = Card(
-                        token = response['data']['card']['token'],
-                        card_type = response['data']['card']['type'],
-                        card_expiry = response['data']['card']['expiry'],
-                        country = response['data']['card']['country']
-                    )
-                    card.save()
+                    card = Card.query.filter_by(token = response['data']['card']['token']).first()
                     
-                    # store user details for login and charge with token use case
-                    user = User(
-                        email = response['data']['customer']['email'],
-                        customer_id = response['data']['customer']['id']
-                    )
-                    user.cards.append(card)
-                    user.save()
+                    if card is None:
+                        card = Card(
+                            user_id = current_user.id,
+                            token = response['data']['card']['token'],
+                            card_type = response['data']['card']['type'],
+                            card_expiry = response['data']['card']['expiry'],
+                            country = response['data']['card']['country']
+                        )
+                        card.save()
+                    
+                    
+                    if current_user.customer_id is None:
+                        # update user table with customer_id
+                        current_user.customer_id = response['data']['customer']['id']
+    
+                        current_user.save()
+                    
                     
                     # store transaction ref to check if costumers have any issue with a payment
                     transaction = Transaction(
                         card_id = card.id,
-                        user_id = user.id,
+                        user_id = current_user.id,
                         transaction_ref = transactionRef,
-                        status = response['data']['status']
+                        status = response['data']['status'],
+                        amount = response['data']['amount_settled']
                     )
                     transaction.save()
                     
-                    return render_template('response.html', home=url_for('payment.payment_view'))
+                    return render_template('response.html', home=url_for('user.payment_view'))
                 else:
                     flash('Not enough funds to cover the transaction')
-                    return redirect('payment.payment_view')
+                    return redirect('user.payment_view')
             else:
                 flash('Invalid Transaction')
-                return redirect('payment.payment_view')
+                return redirect('user.payment_view')
