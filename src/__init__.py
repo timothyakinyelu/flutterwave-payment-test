5ac384e0d7876502439ec07799d8671a84c3bed7
loverofMush <tolujamal1@gmail.com>
2021-02-22 20:18:46 +0100

    - removed duplicate code

--- src/__init__.py
+++ src/__init__.py
@@ -28,55 +28,6 @@ def createApp():
             return User.query.get(user_id)
         return None
 
-    
-    @app.route('/create-subaccount')
-    def createSubAccount():
-        data = { 
-            "account_bank": "044",
-            "account_number": "0690000039",
-            "business_name": "Grape Scotch",
-            "business_email": "petya@stu.net",
-            "business_contact": "Anonymous",
-            "business_contact_mobile": "090820382",
-            "business_mobile": "09083930450",
-            "country": "NG",
-            "split_type": "percentage",
-            "split_value": 0.1
-        }
-        
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {}'.format(current_app.config['SEC_KEY'])
-        }
-        url = 'https://api.flutterwave.com/v3/subaccounts'
-        
-        res = requests.post(url, data=json.dumps(data), headers=headers)
-        response = res.json()
-        
-        return response
-        
-        if resp['status'] == 'success':
-            res = requests.get(url, headers=headers)
-            response = res.json()
-            
-            return response
-        else:
-            res = {'msg': 'Account already exists!'}
-            return jsonify(res)
-        
-
-    @app.route('/subaccounts')
-    def subaccounts():
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {}'.format(current_app.config['SEC_KEY'])
-        }
-        fetchUrl = 'https://api.flutterwave.com/v3/subaccounts'
-        
-        res = requests.get(fetchUrl, headers=headers)
-        response = res.json()
-        
-        return response
         
 
     @app.route('/update-subaccount/<int:id>')
