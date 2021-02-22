8c4172616e25b99a3ef4aafd00cae9ac687c7509
loverofMush <tolujamal1@gmail.com>
2021-02-22 19:22:13 +0100

    - created VendorView class

--- src/views/admin/admin_route.py
+++ src/views/admin/admin_route.py
@@ -11,11 +11,28 @@ class Admin_view(MethodView):
         return AdminController.admin()
     
     
+class VendorView(MethodView):
+    """ Class to conduct all vendor get and post actions"""
+    
+    def get(self):
+        """ Fetch vendor creation template form"""
+        return AdminController.create_vendor()
+    
+    def post(self):
+        """Submit vendor creation form"""
+        pass
+    
+      
 # define API resources
 admin_view = Admin_view.as_view('admin_view')
+vendor_view = VendorView.as_view('vendor_view')
 
 # add url_rule for endpoints
 admin.add_url_rule(
     '/admin',
     view_func=admin_view
+)
+admin.add_url_rule(
+    '/create-vendor',
+    view_func=vendor_view
 )
